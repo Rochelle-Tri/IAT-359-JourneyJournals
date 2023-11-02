8f3822f7e1a5b681d29bfb9e853e5f2f38214789
Arielle Beltran <arielle.appled@gmail.com>
2023-11-02 08:42:37 -0700

    test confirmation
    works

--- app/src/main/java/com/example/journeyjournals/MainActivity.java
+++ app/src/main/java/com/example/journeyjournals/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //comment comment
+        //yes it worksssss
     }
 
     //hello can u see my comment
