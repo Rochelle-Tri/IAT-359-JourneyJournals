ca2784cd4d78d3b789749b6f8e8e52e3cf072773
Rochelle Tri <105394072+Rochelle-Tri@users.noreply.github.com>
2023-11-05 17:50:57 -0800

    renamed files for better understanding

--- app/src/main/java/com/example/journeyjournals/JourneyAlbumRecyclerActivity.java
+++ app/src/main/java/com/example/journeyjournals/JourneyAlbumRecyclerActivity.java
@@ -9,6 +9,6 @@ public class RecyclerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recycler);
+        setContentView(R.layout.activity_journey_album_recycler);
     }
 }
