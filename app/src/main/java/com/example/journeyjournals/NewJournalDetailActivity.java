d0ad6c8e21182880467315813d9af7c8cd4a805b
Arielle Beltran <arielle.appled@gmail.com>
2023-11-08 03:05:48 -0800

    Created the detailed view & layout for new user journey
    -added java & xml file

---
+++ app/src/main/java/com/example/journeyjournals/NewJournalDetailActivity.java
@@ -0,0 +1,14 @@
+package com.example.journeyjournals;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewJournalDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_journey_detail);
+    }
+}
