9f3bb2cbba24056a7054ccedeaaa4706e0da5303
Rochelle Tri <105394072+Rochelle-Tri@users.noreply.github.com>
2023-11-16 23:25:42 -0800

    added initial code for setting up inserting and reading data from database (is still incomplete)

--- app/src/main/java/com/example/journeyjournals/NewJournalDetailActivity.java
+++ app/src/main/java/com/example/journeyjournals/NewJournalDetailActivity.java
@@ -1,14 +1,44 @@
 package com.example.journeyjournals;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NewJournalDetailActivity extends AppCompatActivity {
 
+    EditText journeyName, journeyLocation, journeyDate, journeyDuration, journeyNotes;
+    MyDataBase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_journey_detail);
+
+        journeyName = (EditText)findViewById(R.id.userJourneyNameTextView);
+        journeyLocation = (EditText)findViewById(R.id.userJourneyLocationTextView);
+        journeyDate = (EditText)findViewById(R.id.userJourneyDateTextView);
+        journeyDuration = (EditText)findViewById(R.id.userJourneyTimeTextView);
+        journeyNotes = (EditText)findViewById(R.id.notesBoxTextView);
+    }
+    public void saveData (View view) {
+        String name = journeyName.getText().toString();
+        String location = journeyLocation.getText().toString();
+        String date = journeyDate.getText().toString();
+        String duration = journeyDuration.getText().toString();
+        String notes = journeyNotes.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        long id = db.insertData(name, location, date, duration, notes);
+        if (id < 0)
+        {
+            Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/journeyjournals/NewJourneyActivity.java
+++ app/src/main/java/com/example/journeyjournals/NewJourneyActivity.java
@@ -2,13 +2,50 @@ package com.example.journeyjournals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewJourneyActivity extends AppCompatActivity {
+
+    EditText journeyName, journeyLocation, journeyChecklist;
+    MyDataBase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_journey);
+        journeyName = (EditText)findViewById(R.id.userJourneyNameTextView);
+        journeyLocation = (EditText)findViewById(R.id.userJourneyLocationTextView);
+        journeyChecklist = (EditText)findViewById(R.id.checklistBoxTextView);
+
+        db = new MyDataBase(this);
     }
+
+    public void viewJourneyDetails (View view) {
+        String name = journeyName.getText().toString();
+        String location = journeyLocation.getText().toString();
+        String checkList = journeyChecklist.getText().toString();
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
+        long id = db.insertInitialData(name, location, checkList);
+        if (id < 0)
+        {
+            Toast.makeText(this, "fail", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "success", Toast.LENGTH_SHORT).show();
+        }
+        journeyName.setText("");
+        journeyLocation.setText("");
+        journeyChecklist.setText("");
+
+        //go to details view of this journey
+        Intent intent = new Intent(this, NewJournalDetailActivity.class);
+        Toast.makeText(this, "go to recyclerView",Toast.LENGTH_SHORT).show();
+        startActivity(intent);
+    }
+
 }
