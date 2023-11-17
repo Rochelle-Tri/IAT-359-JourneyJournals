7921e5f855945feaa62a6e7ac1e9936a12472a15
Rochelle Tri <105394072+Rochelle-Tri@users.noreply.github.com>
2023-11-16 23:25:00 -0800

    added primary initial code for setting up the recycler view (album page)

--- app/src/main/java/com/example/journeyjournals/CustomAdapter.java
+++ app/src/main/java/com/example/journeyjournals/CustomAdapter.java
@@ -1,4 +1,78 @@
 package com.example.journeyjournals;
 
-public class CustomAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+//this class creates the actual view from the recycler.
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    public ArrayList<String> list;
+
+    public CustomAdapter(ArrayList<String> list) {
+        this.list = list;
+    }
+
+    @Override
+    public CustomAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.journey_album_layout,parent,false);
+        MyViewHolder viewHolder = new MyViewHolder(v);
+        return viewHolder;
+    }
+
+    //display the thumbnail info from the database into the recycler arraylist
+    @Override
+    public void onBindViewHolder(CustomAdapter.MyViewHolder holder, int position) {
+
+        String[]  results = (list.get(position).toString()).split(",");
+        holder.albumJourneyNameTextView.setText(results[0]);
+        holder.albumJourneyLocationTextView.setText(results[1]);
+        holder.albumDateTextView.setText(results[2]);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public TextView albumJourneyNameTextView;
+        public TextView albumJourneyLocationTextView;
+        public TextView albumDateTextView;
+        public LinearLayout myLayout;
+
+        Context context;
+
+        //display the "thumbnail" info of each journey entry in the album view
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            myLayout = (LinearLayout) itemView;
+
+            albumJourneyNameTextView = (TextView) itemView.findViewById(R.id.albumJourneyNameTextView);
+            albumJourneyLocationTextView = (TextView) itemView.findViewById(R.id.albumJourneyLocationTextView);
+            albumDateTextView = (TextView) itemView.findViewById(R.id.albumDateTextView);
+
+            itemView.setOnClickListener(this);
+            context = itemView.getContext();
+
+        }
+
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(context,
+                    "You have clicked: " + ((TextView)view.findViewById(R.id.albumJourneyNameTextView)).getText().toString(),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/com/example/journeyjournals/JourneyAlbumRecyclerActivity.java
+++ app/src/main/java/com/example/journeyjournals/JourneyAlbumRecyclerActivity.java
@@ -1,14 +1,61 @@
 package com.example.journeyjournals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 
-public class JourneyAlbumRecyclerActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class JourneyAlbumRecyclerActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    private RecyclerView myRecycler;
+    private MyDataBase db;
+    private CustomAdapter customAdapter;
+    private MyHelper helper;
+    private LinearLayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journey_album_recycler);
+        myRecycler = (RecyclerView) findViewById(R.id.rvItems);
+
+        db = new MyDataBase(this);
+        helper = new MyHelper(this);
+        Cursor cursor = db.getData();
+
+       //get the info from each column
+        int index1 = cursor.getColumnIndex(Constants.NAME);
+        int index2 = cursor.getColumnIndex(Constants.LOCATION);
+        int index3 = cursor.getColumnIndex(Constants.DATE);
+
+        //put info into the arraylist that the recycler will use to display the info
+        ArrayList<String> myArrayList = new ArrayList<String>();
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            String journeyName = cursor.getString(index1);
+            String journeyLocation = cursor.getString(index2);
+            String journeyDate = cursor.getString(index3);
+            String s = journeyName +"," + journeyLocation + "," + journeyDate;
+            myArrayList.add(s);
+            cursor.moveToNext();
+        }
+        customAdapter = new CustomAdapter(myArrayList);
+        myRecycler.setAdapter(customAdapter);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        myRecycler.setLayoutManager(mLayoutManager);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
     }
 }
