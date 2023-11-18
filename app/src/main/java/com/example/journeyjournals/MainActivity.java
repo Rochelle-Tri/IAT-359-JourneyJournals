f81876730922fc7b7014cee7f0b1202bc5ddefb7
Arielle Beltran <arielle.appled@gmail.com>
2023-11-17 20:12:34 -0800

    Started sharedpreferences

--- app/src/main/java/com/example/journeyjournals/MainActivity.java
+++ app/src/main/java/com/example/journeyjournals/MainActivity.java
@@ -1,21 +1,60 @@
 package com.example.journeyjournals;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView welcomeUser;
+     final int REQUEST_CODE = 0;
+     String firstName_entered;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        welcomeUser = (TextView) findViewById(R.id.welcomeUser);
         //comment comment
         //yes it worksssss
     }
 
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(requestCode==REQUEST_CODE){
+            if(resultCode==RESULT_OK && data != null && data.hasExtra("First Name")){
+                if ((data.hasExtra("First Name")));
+                {
+//                    String firstname_entered = data.getExtras().getString("First Name");
+//                    welcomeUser.setText("Welcome" +firstname_entered);
+                    String firstName_entered = data.getStringExtra("First Name");
+                    welcomeUser.setText("Welcome " + firstName_entered + "!");
+                }
+
+            }
+        }
+        super.onActivityResult(requestCode,resultCode,data);
+    }
+
+    private void retrieveUserSetting(){
+        Intent i = new Intent(this, UserSettings.class);
+        ActivityCompat.startActivityForResult(this, i, REQUEST_CODE, null);
+    }
+
+//    private void welcomeUserData(){
+//        if (firstName_entered!= null){
+//            welcomeUser.setText("Welcome " + firstName_entered);
+//        }
+//    }
+    protected void onResume(){
+        super.onResume();
+//        welcomeUserData();
+    }
     public void gotoAllTrips(View view){
         Intent i = new Intent(this, JourneyAlbumRecyclerActivity.class);
         startActivity(i);
@@ -27,8 +66,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void gotoUserSettings(View view){
-        Intent i = new Intent(this, UserSettings.class);
-        startActivity(i);
+//        Intent i = new Intent(this, UserSettings.class);
+//        startActivity(i);
+        retrieveUserSetting();
     }
 
+//    @Override
+//    public void onClick(View view) {
+//
+//    }
 }
--- app/src/main/java/com/example/journeyjournals/UserSettings.java
+++ app/src/main/java/com/example/journeyjournals/UserSettings.java
@@ -3,18 +3,47 @@ package com.example.journeyjournals;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class UserSettings extends AppCompatActivity {
+public class UserSettings extends AppCompatActivity implements View.OnClickListener {
+    private Button saveButton;
+    private EditText firstNameEditText;
+    String firstName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_settings);
+
+        saveButton = (Button)findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(this);
+
+        firstNameEditText = (EditText)findViewById(R.id.firstNameEditText);
+
     }
 
-    public void goBack(View v){
+//    public void saveData(View view){
+//        Intent i = new Intent(this, MainActivity.class);
+//        startActivity(i);
+//    }
+
+    public void goBack(View view){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+
+    @Override
+    public void onClick(View view) {
+        firstName = firstNameEditText.getText().toString();
+        Toast.makeText(this, firstName, Toast.LENGTH_SHORT).show();
+
+        Intent retrieveIntent = getIntent();
+        retrieveIntent.putExtra("First Name", firstName);
+        setResult(RESULT_OK, retrieveIntent);
+        finish();
     }
 }
